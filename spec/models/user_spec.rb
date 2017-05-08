f830f6fb042219352e4d5217d4d9ee35d4e6724c
DeliSauce <nycdelisauce@gmail.com>
2017-05-08 13:42:43 -0700

    Update user rspec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before(:each) do
-    User.create!(username: 'test', password: 'password', email: 'test@example.com')
-  end
+  #Needed for uniqueness validation (replaced by 'let', will not persist user to database)
+  # before(:each) do
+  #   User.create!(username: 'test', password: 'password', email: 'test@example.com')
+  # end
 
   describe 'validations' do
+    let!(:user) { User.create!(username: 'test', password: 'password', email: 'test@example.com') }
     it { should validate_presence_of(:username) }
     it { should validate_uniqueness_of(:username) }
     it { should validate_presence_of(:email) }
+    it { should validate_length_of(:password).is_at_least(6) }
     it { should validate_presence_of(:password_digest) }
     it { should validate_presence_of(:session_token) }
   end
