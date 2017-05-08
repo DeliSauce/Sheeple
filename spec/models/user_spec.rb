a2eb2fa7906805689816557f35e823439f2fc799
DeliSauce <nycdelisauce@gmail.com>
2017-05-08 13:07:47 -0700

    Update user rspec to include a pre-existing user for validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,30 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   # let!(:user) { create(:user) }
+  User.create!(username: 'test_user', password: 'password', email: 'test@example.com')
 
   describe 'validations' do
-    it {should validate_presence_of(:username)}
-    it {should validate_uniqueness_of(:username) }
-    it {should validate_presence_of(:email)}
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:session_token) }
   end
 
   describe 'associations' do
-    it {should have_many(:tasks)}
-    it {should have_many(:taskers)}
+    it { should have_many(:tasks) }
+    it { should have_many(:taskers) }
   end
 end
-
-# describe BasketballTeam do
-#   it { should validate_presence_of(:name) }
-#   it { should validate_uniqueness_of(:name) }
-#   it { should ensure_inclusion_of(:name).in_array(["Cavaliers", "Suns", "Mavericks"])} #etc..
-# end
-#
-# describe BasketballTeam do
-#   describe "associations" do
-#     # "it" refers to an instance of the BasketballTeam class here
-#     # because we have not explicitly specified a subject
-#     it { should have_many(:basketball_players)}
-#     it { should have_one(:owner)}
-#   end
-# end
